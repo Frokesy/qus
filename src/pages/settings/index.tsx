0a76c7e463dc893bc803407d3ab92b1ced6f0d1a
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-06-22 10:37:04 +0100

    modified settings to fit mobile screen

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -5,43 +5,47 @@ import { CaretDown } from "../../components/svgs/Icons";
 const Settings = () => {
   return (
     <MainContainer>
-      <h1 className="text-[30px] font-semibold">Settings</h1>
+      <div className="h-[80vh]">
+        <h1 className="lg:text-[30px] text-[20px] font-semibold">Settings</h1>
 
-      <div className="space-y-4 w-[50%] mt-10">
-        <NavLink
-          to="/settings/edit-profile"
-          className="flex items-center justify-between border-b-2 border-[#ccc] pb-4"
-        >
-          <span className="text-[16px]">Edit Account Information</span>
-          <div className="rotate-270">
-            <CaretDown color="black" />
+        <div className="space-y-4 lg:w-[50%] mt-10">
+          <NavLink
+            to="/settings/edit-profile"
+            className="flex items-center justify-between border-b-2 border-[#ccc] pb-4"
+          >
+            <span className="text-[16px]">Edit Account Information</span>
+            <div className="rotate-270">
+              <CaretDown color="black" />
+            </div>
+          </NavLink>
+          <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
+            <span className="text-[16px]">Update Payment Information</span>
+            <div className="rotate-270">
+              <CaretDown color="black" />
+            </div>
           </div>
-        </NavLink>
-        <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
-          <span className="text-[16px]">Update Payment Information</span>
-          <div className="rotate-270">
-            <CaretDown color="black" />
+          <NavLink
+            to="/settings/change-password"
+            className="flex items-center justify-between border-b-2 border-[#ccc] pb-4"
+          >
+            <span className="text-[16px]">Change Password</span>
+            <div className="rotate-270">
+              <CaretDown color="black" />
+            </div>
+          </NavLink>
+          <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
+            <span className="text-[16px]">
+              Enable Two-Factor Authentication
+            </span>
+            <div className="rotate-270">
+              <CaretDown color="black" />
+            </div>
           </div>
-        </div>
-        <NavLink
-          to="/settings/change-password"
-          className="flex items-center justify-between border-b-2 border-[#ccc] pb-4"
-        >
-          <span className="text-[16px]">Change Password</span>
-          <div className="rotate-270">
-            <CaretDown color="black" />
-          </div>
-        </NavLink>
-        <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
-          <span className="text-[16px]">Enable Two-Factor Authentication</span>
-          <div className="rotate-270">
-            <CaretDown color="black" />
-          </div>
-        </div>
-        <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
-          <span className="text-[16px] text-red-500">Logout</span>
-          <div className="rotate-270">
-            <CaretDown color="black" />
+          <div className="flex items-center justify-between border-b-2 border-[#ccc] pb-4">
+            <span className="text-[16px] text-red-500">Logout</span>
+            <div className="rotate-270">
+              <CaretDown color="black" />
+            </div>
           </div>
         </div>
       </div>
