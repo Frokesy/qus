613a612d5f3d5f1542461ef51ba9bb249be4216c
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-06-22 09:16:27 +0100

    modified sidebar to be bottombar for mobile

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -9,7 +9,7 @@ const MainContainer = ({ children }: MainContainerProps) => {
   return (
     <div className="bg-[#f1f1f1]">
       <Topbar />
-      <div className="flex h-[95vh] overflow-hidden">
+      <div className="flex h-[95vh] lg:overflow-hidden overflow-y-auto lg:flex-row flex-col-reverse">
         <Sidebar />
         <div className="w-[85%] p-10">{children}</div>
       </div>
--- src/components/sections/dashboard/Sidebar.tsx
+++ src/components/sections/dashboard/Sidebar.tsx
@@ -10,28 +10,40 @@ import {
 
 const Sidebar = () => {
   return (
-    <div className="w-[15%] flex flex-col justify-between border-r-2 border-[#808080] text-[#667085] pt-3 h-[95vh]">
-      <div className="space-y-6">
-        <NavLink to="/dashboard" className="flex items-center space-x-3  px-4">
+    <div className="lg:w-[15%] bg-[#fff] lg:bg-inherit py-4 w-[100%] flex lg:flex-col flex-row justify-between fixed lg:relative bottom-0 lg:border-r-2 border-[#808080] text-[#667085] lg:pt-10 lg:h-[95vh]">
+      <div className="lg:space-y-6 flex lg:flex-col flex-row justify-between w-[100%]">
+        <NavLink
+          to="/dashboard"
+          className="flex lg:flex-row flex-col items-center space-x-3 px-4"
+        >
           <DashboardIcon />
           <span>Dashboard</span>
         </NavLink>
-        <NavLink to="/tasks" className="flex items-center space-x-3  px-4">
+        <NavLink
+          to="/tasks"
+          className="flex lg:flex-row flex-col items-center space-x-3 px-4"
+        >
           <TaskIcon />
           <span>Tasks</span>
         </NavLink>
-        <NavLink to="/wallet" className="flex items-center space-x-3  px-4">
+        <NavLink
+          to="/wallet"
+          className="flex lg:flex-row flex-col items-center space-x-3 px-4"
+        >
           <CashIcon />
           <span>Wallet</span>
         </NavLink>
-        <NavLink to="/settings" className="flex items-center space-x-3  px-4">
+        <NavLink
+          to="/settings"
+          className="flex lg:flex-row flex-col items-center space-x-3 px-4"
+        >
           <SettingsIcon />
           <span>Settings</span>
         </NavLink>
       </div>
 
-      <div className="space-y-6 pb-20">
-        <div className="flex items-center space-x-3  px-4">
+      <div className="space-y-6 pb-20 lg:block hidden">
+        <div className="flex items-center space-x-3 px-4">
           <SupportIcon />
           <span>Live Support</span>
         </div>
