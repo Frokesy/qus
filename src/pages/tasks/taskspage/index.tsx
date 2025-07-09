07bb9e5b102b70516f942abbf4ca1a6104f97862
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-07-09 14:12:14 +0100

    minor

--- src/pages/tasks/taskspage/index.tsx
+++ src/pages/tasks/taskspage/index.tsx
@@ -104,8 +104,6 @@ const TaskPage = () => {
     );
   }
 
-  const isUploadTask = task.category === "upload";
-
   return (
     <MainContainer>
       <div className="max-w-3xl h-[80vh] overflow-y-auto mx-auto lg:py-10 space-y-6 text-white">
@@ -122,28 +120,6 @@ const TaskPage = () => {
         </p>
 
         <p className="text-gray-400">{task.desc}</p>
-
-        {isUploadTask ? (
-          <div className="border-2 border-dashed border-gray-600 p-6 rounded-lg flex flex-col items-center justify-center">
-            <input
-              type="file"
-              accept="image/*"
-              className="mb-4 text-sm text-white"
-            />
-            <button className="bg-blue-600 w-[100%] text-center text-white px-5 py-2 rounded-md hover:bg-blue-700">
-              Upload Evidence
-            </button>
-          </div>
-        ) : (
-          <a
-            href={task.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block bg-blue-600 text-center w-[100%] hover:bg-blue-700 transition px-6 py-3 rounded-md text-white font-medium"
-          >
-            🔗 {task.cta}
-          </a>
-        )}
 
         <button
           onClick={() => handleCompleteTask(task.id.toString(), task.reward)}
